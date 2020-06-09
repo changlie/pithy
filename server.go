3bf9cf64afef5f6987fd88bc0568f069d2cd927c
changlie <583491331@qq.com>
2020-06-09 18:03:19 +0800

    Update server.go

--- server.go
+++ server.go
@@ -1,23 +1,50 @@
 package pithy
 
 import (
-  // "io"
-  "log"
-  "net/http"
-  "os/exec"
-  "time"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"time"
 )
 
+var devMode = false
+
+func StartInDevMode() {
+	devMode = true
+	Start()
+}
+
 func Start() {
-  http.HandleFunc("/", mainHandler)
-  log.Fatal(http.ListenAndServe(":8888", nil))
+	if !devMode {
+		os.Chdir(filepath.Dir(os.Args[0]))
+	}
+
+	http.HandleFunc("/", mainHandler)
+	port := "8888"
+	addr := fmt.Sprintf(":%s", port)
+	log.Printf("server [%v] start up successfully!", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func Pwd() string {
+	dir, _ := os.Getwd()
+	return dir
 }
 
 func OpenBrowser(url string) {
-  go func() {
-    time.Sleep(time.Second)
+	go func() {
+		time.Sleep(time.Second)
 
-    cmd := exec.Command("xdg-open", url)
-    cmd.Run()
-  }()
+		var cmd *exec.Cmd
+		if runtime.GOOS == "windows" {
+			cmd = exec.Command("cmd", "/C", "start", url)
+		} else {
+			cmd = exec.Command("xdg-open", url)
+		}
+		cmd.Run()
+	}()
 }

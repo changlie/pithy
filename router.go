1132eb28fb82030b622d09c396da9a39f12117e4
changlie <583491331@qq.com>
2020-06-09 18:02:25 +0800

    Update router.go

--- router.go
+++ router.go
@@ -1,143 +1,186 @@
 package pithy
 
 import (
-  "bufio"
-  "net/http"
-  "fmt"
-  "strings"
+	"bufio"
+	"errors"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func mainHandler(w http.ResponseWriter, r *http.Request)  {
-  printReqInfo(r)
-  url := r.URL.RawPath
-  queryArgs := r.URL.Query()
-  headers := r.Header
+	printReqInfo(r)
+	url := r.URL.Path
 
-  req := Req{queryArgs, headers}
+	// if Accessing local file, return local file directly
+	fp, err := localFile(url)
+	if err == nil {
+		returnLocalFile(w, fp)
+		return
+	}
 
-  h := getHandler(url)
-  if h == nil {
-    w.WriteHeader(404)
-    fmt.Fprintln(w, "service is not found!")
-    return
-  }
-  resp := h(req)
-  handleResp(w, resp)
+	log.Printf("string(): %v; path: %v; rawpath: %v \n", r.URL, r.URL.Path, r.URL.RawPath)
+	queryArgs := r.URL.Query()
+	headers := r.Header
 
+	req := Req{Args:queryArgs, Header:headers}
+
+	h := getHandler(url)
+	if h == nil {
+		NotFound(w)
+		return
+	}
+	resp := h(req)
+	handleResp(w, resp)
+}
+
+func NotFound(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintln(w, "service is not found!")
+}
+
+func InternalServerError(w http.ResponseWriter, e error) {
+	w.WriteHeader(http.StatusInternalServerError)
+	fmt.Fprintln(w, "server error: "+e.Error())
 }
 
 func handleResp(w http.ResponseWriter, resp *Resp) {
-  if resp.isJson() {
-    var res string
-    switch val := resp.data.(type) {
-    case string:
-      res = val
-    default:
-      res = "not support temp"
-    }
-    fmt.Fprint(w, res)
-    return
-  }
-  bufio.NewWriter(w).Write([]byte("not support temp"))
+	if resp.isJson() {
+		var res string
+		switch val := resp.data.(type) {
+		case string:
+			res = val
+		default:
+			res = "not support temp"
+		}
+		fmt.Fprint(w, res)
+		return
+	}
+
+	if resp.isFile() {
+		path, ok := resp.data.(string)
+		if !ok {
+			NotFound(w)
+			return
+		}
+		fp, err := localFile(path)
+		if err != nil {
+			NotFound(w)
+			return
+		}
+		returnLocalFile(w, fp)
+
+		return
+	}
+
+	bufio.NewWriter(w).Write([]byte("not support temp"))
+}
+
+func localFile(path string) (string, error) {
+	if path == "" || path == "/" {
+		return "", errors.New("don't handle root path")
+	}
+	fp := filepath.Join(Pwd(), "static", path)
+	_, err := os.Open(fp)
+	return fp, err
+}
+
+func returnLocalFile(w http.ResponseWriter, fp string) {
+	data, err := ioutil.ReadFile(fp)
+	if err != nil {
+		InternalServerError(w, err)
+		return
+	}
+	bw := bufio.NewWriter(w)
+	bw.Write(data)
+	bw.Flush()
 }
 
 func printReqInfo(r *http.Request) {
-  fmt.Println(r.URL)
-  fmt.Println("head:---------------------")
-  for k, v := range r.Header {
-    fmt.Println(k, v)
-  }
-}
-
-type uri struct {
-  url string
-  // subUrls []string
+	fmt.Println(r.URL)
+	fmt.Println("head:---------------------")
+	for k, v := range r.Header {
+		fmt.Println(k, v)
+	}
 }
 
 var (
-  Urlmap = make(map[uri]Handler)
+	urlmap = make(map[string]Handler)
 )
 
-func getHandler(url string) Handler {
-  u := NewUri(url)
-  for p, h := range Urlmap {
-    if p.equal(u) {
-      return h
-    }
-  }
-  return nil
+func getHandler(url string) HandleFunc {
+	for p, h := range urlmap {
+		if p == url {
+			return h.Action
+		}
+	}
+	log.Printf("current urlmap: %v \n", urlmap)
+	log.Printf("service is not found for %v \n", url)
+
+	return nil
 }
 
-func SetHandler(url string, h Handler) {
-  u := NewUri(url)
-  Urlmap[u] = h
+func SetHandler(url string, hf HandleFunc) {
+	paths := strings.Split(url, "/")
+
+	urlmap[url] = Handler{Paths:paths, Action:hf}
 }
 
+type HandleFunc func(r Req) *Resp
 
-func NewUri(url string) uri {
-  paths := strings.Split(url, "/")
-  return uri{url, paths}
+type Handler struct {
+	Paths []string
+	Action HandleFunc
 }
-
-func (u *uri) equal(target uri) bool {
-  subUrls1 := target.subUrls
-  for i, subUrl := range u.subUrls {
-    if i >= len(subUrls1) {
-      return false
-    }
-    subUrl1 := subUrls1[i]
-    if subUrl != subUrl1 && !strings.HasPrefix(subUrl, ":") {
-      return false
-    }
-  }
-  return true
-}
-
-type Handler func(r Req) *Resp
 
 type Req struct {
-  Args map[string][]string
-  Header http.Header
+	Args map[string][]string
+	Header http.Header
 }
 
 func (r *Req) getArg(name string) []string {
-  return r.Args[name]
+	return r.Args[name]
 }
 
+
 type Resp struct {
-  data interface{}
-  t respType
+	data interface{}
+	t respType
 }
 
 type respType int
 
 const (
-  respJson respType = 1 << iota
-  respFile
+	respJson respType = 1 << iota
+	respFile
 )
 
 func RespJson(d interface{}) *Resp {
-  return &Resp{
-    data: d,
-    t: respJson,
-  }
+	return &Resp{
+		data: d,
+		t: respJson,
+	}
 }
 
 func RespFile(path string) *Resp {
-  return &Resp{
-    data: path,
-    t: respFile,
-  }
+	return &Resp{
+		data: path,
+		t: respFile,
+	}
 }
 
 func (resp *Resp) isJson() bool {
-  return (resp.t & respJson) != 0
+	return (resp.t & respJson) != 0
 }
 
 func (resp *Resp) isFile() bool {
-  return (resp.t & respFile) != 0
+	return (resp.t & respFile) != 0
 }
 
 type Json interface {
-  JsonString() string
+	JsonString() string
 }
